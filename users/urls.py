c9b42e50fdf4cc4744e759da3e61bb9b57d82311
antongulikov <contest.gulikov@gmail.com>
2015-05-06 05:02:16 +0000

    add paginator

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url, include
-from views import userPage
+from views import userPage, userProblem
 
-urlpatterns = [ url(r'(?P<username>[\w.@+-]+)', userPage)]
+urlpatterns = [
+            url(r'(?P<username>[\w.@+-]+)/problems/page/(?P<page_number>[\d+]+)/$', userProblem),
+            url(r'(?P<username>[\w.@+-]+)/problems/$', userProblem),
+            url(r'(?P<username>[\w.@+-]+)', userPage)
+                ]
--- users/views.py
+++ users/views.py
@@ -2,14 +2,26 @@ from django.shortcuts import render
 from django.http.response import Http404, HttpResponse
 from django.shortcuts import render_to_response, redirect
 from users.models import UpUser
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+def userProblem(request, username, page_number = 1):
+    try:
+        args = {}
+        user = UpUser.objects.get(username=username)
+        args['user'] = user.username
+        all_problems = user.problem_set.all()
+        current_page = Paginator(all_problems, 20)
+        args['problems'] = current_page.page(page_number)
+        return render_to_response('problemsPage.html', args)
+    except:
+        return redirect('/')
 
 def userPage(request, username):
     try:
         args = {}
         user = UpUser.objects.get(username=username)
-        args['problems'] = user.problem_set.all()
         args['user'] = user.username
         return render_to_response('userPage.html', args)
     except:
