873ba573c4d69d633b0dc65f15667c0e9af1ec54
Anton Gulikov <contest.gulikov@gmail.com>
2015-05-08 20:21:21 +0300

    users fiexd

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url, include
-from views import userPage
+from views import userPage, userProblem
 
-urlpatterns = [ url(r'(?P<username>[\w.@+-]+)', userPage)]
+urlpatterns = [
+            url(r'(?P<username>[\w.@+-]+)/problems/(\d*)', userProblem),
+            url(r'(?P<username>[\w.@+-]+)', userPage)
+                ]
