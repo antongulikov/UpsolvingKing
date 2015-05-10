ad596f0c59de84e83cc28f5681a236c122ecee6f
Anton Gulikov <contest.gulikov@gmail.com>
2015-05-10 19:49:38 +0300

    change mainpage

--- mainpage/views.py
+++ mainpage/views.py
@@ -7,6 +7,6 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def mainPage(request):
-    return render_to_response('mainPage.html', {'user': auth.get_user(request).username})
+    return render_to_response('mainPage.html', {'user': auth.get_user(request).username, 'main' : True})
 
 
